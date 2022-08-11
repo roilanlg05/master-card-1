8c94ae406ca83a28e0d7146d3333593cef498dfb
roilan.lambert5@gmail.com <roilan.lambert5@gmail.com>
2022-08-11 01:25:01 -0400

    primera-prueba

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+Vue.createApp({
+  data(){
+    return{
+      search: '',
+      products: null
+    }
+  },
+  created(){
+    this.fetchData('api.json')
+    this.prueba()
+  },
+  methods:{
+    async fetchData(API){
+      try {
+        let res = await fetch(API)
+        let products = await res.json()
+        console.log(products)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    
+    prueba(){
+      
+      let a =[a,s,d,r,g,h]
+      let n =[g,a,d,g,y,g]
+      let i =[]
+      
+      for (d,b in a,n){
+        if(d==b){
+          i.push(d)
+        }
+      }
+      console.log(i)
+    }
+  }
+}).mount('#app')
